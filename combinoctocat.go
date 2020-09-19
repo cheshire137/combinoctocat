46ddb9e7c16c7d475c30b0e0473d74c02e89a21a
Sarah Vessels <cheshire137@gmail.com>
2020-09-19 18:31:56 -0500

    Start grabbing container elements

--- combinoctocat.go
+++ combinoctocat.go
@@ -31,4 +31,29 @@ func main() {
 		os.Exit(1)
 		return
 	}
+
+	bodyColorNode := models.GetElementById(node, "body-color")
+
+	eyeStyleNode := models.GetElementById(node, "cp-eyes")
+	eyeColorNode := models.GetElementById(node, "eye-color")
+
+	faceColorNode := models.GetElementById(node, "face-color")
+
+	hairStyleNode := models.GetElementById(node, "cp-hair")
+	hairColorNode := models.GetElementById(node, "hair-color")
+
+	headgearNode := models.GetElementById(node, "cp-headgear")
+
+	topNode := models.GetElementById(node, "cp-tops")
+
+	bottomNode := models.GetElementById(node, "cp-bottoms")
+
+	footwearNode := models.GetElementById(node, "cp-footwear")
+
+	eyewearNode := models.GetElementById(node, "cp-eyewear")
+
+	propNode := models.GetElementById(node, "cp-props")
+
+	facialHairStyleNode := models.GetElementById(node, "cp-faceHair")
+	facialHairColorNode := models.GetElementById(node, "facehair-color")
 }
